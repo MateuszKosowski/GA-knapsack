3bfa0e46717b49eafb66ced4bb25724fc93e6870
Jakub Rosiak <251620@edu.p.lodz.pl>
2025-11-11 14:21:40 +0100

    feat: algorithm

--- ga_knapsack.py
+++ ga_knapsack.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import random
 
 items_data = pd.read_csv('data/problem_plecakowy_dane_CSV_tabulatory.csv', sep='\t')
 
@@ -9,11 +10,9 @@ items_data = items_data.iloc[:, 1:]
 for c in items_data.columns[1:]:
     items_data[c] = pd.to_numeric(items_data[c].str.replace(' ', ''), errors='coerce')
 
-print(items_data)
-
 COUNT_OF_ITEMS = len(items_data)
 BACKPACK_LOAD_CAPACITY = 6404180
-
+COUNT_OF_ITERATIONS = 100
 class Knapsack:
     def __init__(self, population_size, cross_probability, mutation_probability):
         self.population_size = population_size
@@ -44,7 +43,7 @@ class Knapsack:
 
 
     def roulette(self):
-        print(self.population)
+        # print(self.population)
 
         chromosomes = [item[0] for item in self.population]
         fitness_scores = [item[1] for item in self.population]
@@ -54,6 +53,11 @@ class Knapsack:
             weights=fitness_scores,
             k=1
         )
+
+        print("selected: ")
+        print(selected)
+        print("\n")
+
         return selected[0]
 
     def signle_cross(self, chromosome_1, chromosome_2):
@@ -62,7 +66,7 @@ class Knapsack:
         crossover_point = random.randint(1, COUNT_OF_ITEMS - 1)
 
         child_1 = np.concatenate([chromosome_1[:crossover_point], chromosome_2[crossover_point:]])
-        child_2 = np.concatenate([chromosome_1[crossover_point:], chromosome_2[:crossover_point]])
+        child_2 = np.concatenate([chromosome_2[:crossover_point], chromosome_1[crossover_point:]])
 
         return child_1, child_2
 
@@ -82,21 +86,36 @@ class Knapsack:
             parent_1 = self.roulette()
             parent_2 = self.roulette()
 
-        if random.random() < self.cross_probability:
-            child_1, child_2 = self.signle_cross(parent_1, parent_2)
-        else:
-            child_1, child_2 = parent_1.copy(), parent_2.copy()
-
-        child_1 = self.mutate_bit_flip(child_1)
-        child_2 = self.mutate_bit_flip(child_2)
-
-        new_population.append(child_1)
-        new_population.append(child_2)
+            # print("parent: \n")
+            # print(parent_1)
 
 
-knapsack = Knapsack(10)
-print(knapsack.population)
+            if random.random() < self.cross_probability:
+                child_1, child_2 = self.signle_cross(parent_1, parent_2)
+            else:
+                child_1, child_2 = parent_1.copy(), parent_2.copy()
+            # print(child_1)
+            child_1 = self.mutate_bit_flip(child_1)
+            child_2 = self.mutate_bit_flip(child_2)
+
+            child_1 = self.calculate_fitness(child_1)
+            child_2 = self.calculate_fitness(child_2)
+
+            new_population.append(child_1)
+            new_population.append(child_2)
+
+        self.population = new_population
 
 
-knapsack.generate_initial_population()
-knapsack.roulette()
+def main():
+    knapsack = Knapsack(10, 0.9, 0.5)
+    knapsack.generate_initial_population()
+    # print(knapsack.population)
+
+    for _ in range(COUNT_OF_ITERATIONS):
+        knapsack.create_new_population()
+        #print(knapsack.population)
+
+
+if __name__ == '__main__':
+    main()
